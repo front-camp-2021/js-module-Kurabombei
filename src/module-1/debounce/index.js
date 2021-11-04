6736019d8fbab2f7d0741cd7e93e816b3b7ea759
Volos Ivan <ivan.volos1@gmail.com>
2021-11-04 18:47:09 +0200

    js-module #1 homework is done

--- src/module-1/debounce/index.js
+++ src/module-1/debounce/index.js
@@ -1,5 +1,19 @@
 export const debounce = (fn, delay = 0) => {
+  // Вся задача зводиться до наставлення та обнулення timeout
+  let timeout;
+  return function () {
+    // Створили функцію,яку будемо викликати після таймауту, яка викликатиме fn
+    // з вказаним контекстом, та переданими аргументами.
+    let fnCall = () => {
+      fn.apply(this, arguments);
+    };
 
+    // Якщо вже є проводиться затримка з timeout, то ми її обнулюємо
+    clearTimeout(timeout);
+
+    // Запускаємо нову затримку, та закидаємо timeoutID з setTimeout в змінну timeout
+    timeout = setTimeout(fnCall, delay);
+  }
 }
 
 
--- src/module-1/weirdString/index.js
+++ src/module-1/weirdString/index.js
@@ -1,1 +1,3 @@
-export const weirdString = (str = "") => {};
+export const weirdString = (str = "") => {
+  return str.split(' ').map((el) => el.substring(0, el.length-1).toUpperCase() + el.charAt(el.length-1).toLowerCase()).join(' ');
+};
